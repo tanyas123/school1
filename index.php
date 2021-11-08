71f3cc6ebd4c5c41b99b73fcc73e363a292109be
tanyas123 <tatyana-sirotina@mail.ru>
2021-11-08 13:22:00 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$name = 'Tanya';
+
+$hello = "Здравствуй, $name";
+$info = '$name - это имя пользователя';
+$a = 88;
+$b = 55;
+echo '<pre>';
+echo $hello;
+echo '<br>';
+echo $info;
+echo '<br>';
+echo '$a + $b = $c';
+echo '<br>';
+echo '$a = ' ,"$a";
+echo "\$a = $a";
+echo '<br>';
+echo '$b = ', "$b";
+echo '<br>';
+echo "$a + $b = ";
+echo $a+$b;
+echo '</pre>';
+
+
